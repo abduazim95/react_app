df64ca8ab6a1859280fcf7144b5289240d29c250
abduazim95 <abduazim95@gmail.com>
2020-01-10 09:36:28 +0600

    Проверка типов с помощью PropTypes

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Redirect, Route } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 const ProtectedRoute = ({ component: Component, auth, ...other }) => (
   <Route {...other} render={(props) => (
@@ -8,5 +9,10 @@ const ProtectedRoute = ({ component: Component, auth, ...other }) => (
       : <Redirect to='/login' />
   )} />
 )
+
+ProtectedRoute.propTypes = {
+  component: PropTypes.elementType,
+  auth: PropTypes.object
+}
 
 export default ProtectedRoute;
